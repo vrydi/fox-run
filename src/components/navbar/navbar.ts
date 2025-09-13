e6841526faa13a2071f8c72151fd7cefe53d49d0
vrydi <vrydifur@gmail.com>
2025-09-13 18:03:06 +0200

    fix mobile navbar

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,7 +1,9 @@
 import { defineComponent } from "vue";
+import { Bars3Icon } from "@heroicons/vue/20/solid";
 
 export default defineComponent({
   name: "Navbar",
+  components: { Bars3Icon },
   setup() {},
   data() {
     return {
